ead09ddc497ee1b89719d69dd0f4150a400acd24
piyushsarkale23 <piyushsarkale35@gmail.com>
2023-04-05 18:27:13 +0530

    Add project files.

---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //****     *          ****             *
+            //***      **         ****            ***
+            //**       ***        ****           *****    
+            //*        ****       ****      
+
+            //int i, j, k, l;
+            //int n = 5;
+            //for (i = 1; i <= n; i++)
+            //{
+            //    for (j = 1; j <= n - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (k = 1; k <= i; k++)
+            //    {
+            //        Console.Write(k);
+            //    }
+            //    for (l = i - 1; l >= 1; l--)
+            //    {
+            //        Console.Write(l);
+            //    }
+            //    Console.Write("\n");
+            //}
+
+            //int n = 5;
+            //int i, j, k, l;
+            //for(i =1;i<=n;i++)
+            //{
+            //    for(j =1;j<= n-i;j++ )
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for(k = 1;k<=i;k++)
+            //    {
+            //        Console.Write(k);
+            //    }
+            //    for(l =i-1;l>=1;l--)
+            //    {
+            //        Console.Write(k);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("Enter a character to check its occurence");
+            //char c = Convert.ToChar(Console.ReadLine());
+            //string s = "hello";
+            //int res = 0;
+            //for(int i=0;i<s.Length;i++)
+            //{
+            //    if (s[i]==c)
+            //    {
+            //        res++;
+            //        Console.WriteLine($"The ocuurence of {c} is {res} times");
+            //    }
+
+            //}
+            //int alph = 0, digi = 0,l,i=0, spc = 0;
+            //string str = "Jack@123";
+            //l = str.Length;
+
+            //while(i<l)
+            //{
+            //    if ((str[i]>='a' && str[i]<='z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            //    {
+            //        alph++;
+            //    }
+            //    else if (str[i]>='0' && str[i]<='9')
+            //    {
+            //        digi++;
+            //    }
+            //    else
+            //    {
+            //        spc++;
+            //    }
+            //    i++;
+
+            // }
+
+            //Console.WriteLine($"The total numbr of alphabets are {alph} and total number of digits are {digi} and Special char is {spc}");
+
+            //int[] arr = { 1, 0, 35,4,5,6,7,8,9,21};
+            //int min = arr[0];
+            //for(int i=0;i<arr.Length;i++)
+            //{
+            //    if (min > arr[i])
+            //    {
+            //        min = arr[i];
+            //    }
+
+            //}
+            //Console.WriteLine("The minimum number is " + min);
+
+
+
+
+
+
+            //Console.WriteLine("enter a number to gets its reverse");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //int rem;
+            //int rev = 0;
+            //int temp = num;
+            //while(num>0)
+            //{
+            //    rem = num % 10;
+            //    rev = rev * 10 + rem;
+            //    num = num / 10;
+            //}
+            //Console.WriteLine($"The reversed number is " + rev);
+
+            //if(temp==rev)
+            //{
+            //    Console.WriteLine("The number is Palindrome");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number is not  Palindrome");
+            //}
+
+
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int alt;
+            //for(int i=0;i<arr.Length;i++) 
+            //{
+            //    for (i = 0; i < arr.Length; i += 2)
+            //    {
+            //        alt = arr[i];
+            //        Console.WriteLine($"The alternate numbers are" + alt);
+            //    }
+
+
+            //}
+
+            //char c;
+            //for(c ='A';c<'Z';c++ )
+            //{
+            //    Console.WriteLine(c);
+            //}
+            
+            //for(char i='A';i<='Z';i++)
+            //{
+            //    for (char j = 'A'; j <= 'Z'; j++)
+            //    {
+            //        Console.Write(j);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+        }
+    }
+}
